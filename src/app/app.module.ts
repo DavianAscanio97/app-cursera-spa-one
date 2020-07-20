711b339b47a7d00d50c05f611e019e4a9068cb68
Davian Ascanio <davianascanio1997@gmail.com>
2020-07-20 11:38:16 -0500

    Módulo 1 - TAREA
    Tareas calificadas por los compañeros: Evaluación del proyecto - Módulo 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RouterModule } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { HomeComponent } from './components/home/home.component';
+import { FormsModule } from '@angular/forms';
+import { ItemsComponent } from './components/items/items.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ItemsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
